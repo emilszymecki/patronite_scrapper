76dbe06bc5ce0be879204aac4309d0cd319c095d
robot-tri-table <97796726+robot-tri-table@users.noreply.github.com>
2022-07-12 14:28:49 +0200

    data_format

--- module.js
+++ module.js
@@ -5,10 +5,10 @@ async function getMembersData(page, args) {
             name = x.querySelector(".card__content--name").innerText.replace(/['"]+/g, ''),
             numbers = [...x.querySelectorAll(".card__content--numbers div")],
             tags_format = tags ? tags.innerText.replaceAll('\n',',') : '',
-            patrons = numbers[0] ? numbers[0].querySelector("span").innerText.replace(/[^0-9]/g, "") : "FALSE",
-            month_amount = numbers[1] ? numbers[1].querySelector("span").innerText.replace(/[^0-9]/g, "") : "FALSE",
-            total_amount = numbers[2] ? numbers[2].querySelector("span").innerText.replace(/[^0-9]/g, "") : "FALSE";
-          
+            patrons = numbers[0] ? numbers[0].querySelector("span").innerText : "FALSE",
+            month_amount = numbers[1] ? numbers[1].querySelector("span").innerText : "FALSE",
+            total_amount = numbers[2] ? numbers[2].querySelector("span").innerText : "FALSE";
+  
             return ({
               tags: tags_format,
               name:name,
